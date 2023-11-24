65385887f2909b8daf2816d4b2d79b8f06dcf5cf
rajaalebchiri <rajaa.lebchiri@gmail.com>
2023-11-24 15:19:32 +0100

    inside python objects 3 4

---
+++ ex4_3/descrip.py
@@ -0,0 +1,13 @@
+# descript.py -- ex 4 3
+class Descriptor:
+    def __init__(self, name):
+        self.name = name
+
+    def __get__(self, instance, cls):
+        print("%s:__get__" % self.name)
+
+    def __set__(self, instance, value):
+        print('%s:__set__ %s' % (self.name, value))
+
+    def __delete__(self, instance):
+        print('%s:__delete__' % self.name)
